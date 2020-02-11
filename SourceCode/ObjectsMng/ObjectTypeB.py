2d5f140e9ed37f62e07711998355cb689ab6c208
Danya8308 <45468840+Danya8308@users.noreply.github.com>
2020-02-11 08:04:29 +0300

    Уже который раз перезаливаю
    Над научиться работать с ветками кстатЕ

---
+++ SourceCode/ObjectsMng/ObjectTypeB.py
@@ -0,0 +1,14 @@
+from Components.Collider import *
+#
+from pygame import *
+
+class ObjectTypeB:
+    #Тэг объекта
+    tag = 'Object'
+
+    #Поле для работы с геометрией объекта
+    geom = Axes()
+
+    #Коллайдер объекта
+    coll = Collider(geom)
+
